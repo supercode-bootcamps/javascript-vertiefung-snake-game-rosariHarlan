dc4dc14942099acad8fc294878415f837829db6a
Rosari Harlan <rosari.harlan@gmail.com>
2021-02-23 15:20:10 +0100

    All done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
-var snakeContainer = document.getElementById("snakeField");
-var item = document.querySelectorAll(".item");
+var snakeContainer = document.createElement("div");
+snakeContainer.id = "snakeField";
+document.body.appendChild(snakeContainer);
 
 function createDivs() {
   for (var i = 0; i <= 99; i++) {
-    snakeContainer.innerHTML += '<div class="item"></div>';
+    var divItems = document.createElement("div");
+    divItems.classList.add("item");
+    snakeContainer.appendChild(divItems);
   }
 }
 
 createDivs();
 
-snakeContainer.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor = "orange";
+snakeContainer.addEventListener("mouseover", (e) => {
+  e.target.style.backgroundColor = "#fe019a";
+  e.target.style.animation = "disco 1.5s infinite";
+  setTimeout(function () {
+    e.target.style.backgroundColor = "lightblue";
+    e.target.style.animation = "none";
+  }, 500);
 });

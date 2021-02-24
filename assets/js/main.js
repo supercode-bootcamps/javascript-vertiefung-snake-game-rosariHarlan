dc3d4cb89e05e4375a291a129bd06c5c646849b8
Rosari Harlan <rosari.harlan@gmail.com>
2021-02-24 21:03:33 +0100

    Add food functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,45 @@
-var snakeContainer = document.createElement("div");
+// --- Create main div container ---
+
+let snakeContainer = document.createElement("div");
 snakeContainer.id = "snakeField";
 document.body.appendChild(snakeContainer);
 
-function createDivs() {
-  for (var i = 0; i <= 99; i++) {
-    var divItems = document.createElement("div");
-    divItems.classList.add("item");
-    snakeContainer.appendChild(divItems);
+// --- Create 100 children divs and mouseover effect ---
+
+function discoSnake() {
+  for (let i = 0; i <= 99; i++) {
+    let divItem = document.createElement("div");
+    divItem.classList.add("item");
+    snakeContainer.appendChild(divItem);
+
+    divItem.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "#fe019a";
+      // e.target.style.animation = "disco 1.5s infinite";
+      setTimeout(function () {
+        e.target.style.backgroundColor = "lightblue";
+        // e.target.style.animation = "none";
+        // e.target.innerHTML = "&#x1F40D;";
+      }, 200);
+    });
   }
 }
 
-createDivs();
+discoSnake();
 
-snakeContainer.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "#fe019a";
-  e.target.style.animation = "disco 1.5s infinite";
-  setTimeout(function () {
-    e.target.style.backgroundColor = "lightblue";
-    e.target.style.animation = "none";
-  }, 500);
-});
+let snakeItem = document.getElementsByClassName("item");
+let score = 0;
+
+let addSnakeFood = () => {
+  let snakeFood = 0;
+  setInterval(function () {
+    snakeFood = Math.floor(Math.random() * 99);
+    snakeItem[snakeFood].classList.add("food");
+    snakeItem[snakeFood].addEventListener("mouseover", (e) => {
+      e.target.classList.remove("food");
+      // score++;
+      // console.log(score);
+    });
+  }, 1000);
+};
+
+addSnakeFood();
